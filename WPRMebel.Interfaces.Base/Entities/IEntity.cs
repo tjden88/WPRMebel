46e142a3f7c42261d9c6d22e0cd0c8b113902a25
Денис <dultsevdv@gmail.com>
2021-10-19 01:34:14 +0500

    Базовые интерфейсы сущностей

---
+++ WPRMebel.Interfaces.Base/Entities/IEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace WPRMebel.Interfaces.Base.Entities
+{
+    public interface IEntity
+    {
+        /// <summary>Идентификатор сущности</summary>
+        public int Id { get; }
+    }
+}
---
+++ WPRMebel.Interfaces.Base/Entities/INamedEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace WPRMebel.Interfaces.Base.Entities
+{
+    public interface INamedEntity : IEntity
+    {
+        /// <summary>Наименование сущности</summary>
+        public string Name { get; }
+    }
+}
