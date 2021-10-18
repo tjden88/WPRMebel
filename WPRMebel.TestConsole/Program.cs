363cb1f0a265ebf082714289349e6393445ecdb9
Денис <dultsevdv@gmail.com>
2021-10-19 00:47:39 +0500

    Добавьте файлы проекта.

---
+++ WPRMebel.TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WPRMebel.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
