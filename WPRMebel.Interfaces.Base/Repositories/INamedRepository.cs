36206ee6835a2f5c6c30d737946a190e060acb27
Денис <dultsevdv@gmail.com>
2021-10-19 04:47:52 +0500

    Интерфейс репозиториев сущностей

---
+++ WPRMebel.Interfaces.Base/Repositories/INamedRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using WPRMebel.Interfaces.Base.Entities;
+
+namespace WPRMebel.Interfaces.Base.Repositories
+{
+    /// <summary>
+    /// Интерфейс репозитория именованных сущностей
+    /// </summary>
+    /// <typeparam name="TNamedEntity">Именованная сущность</typeparam>
+    public interface INamedRepository<TNamedEntity> : IRepository<TNamedEntity> where TNamedEntity : INamedEntity
+    {
+        /// <summary>
+        /// Получить сущность по имени
+        /// </summary>
+        /// <param name="name">Имя искомой сущности</param>
+        /// <returns>null, если сущность не найдена</returns>
+        Task<TNamedEntity> GetByName(string name);
+
+        /// <summary>
+        /// Существует ли сущность в репозитории
+        /// </summary>
+        /// <param name="name">Имя искомой сущность</param>
+        /// <returns>Истина, если сущность есть в репозитории</returns>
+        virtual Task<bool> Exist(string name) => Exist(GetByName(name).Id);
+
+        /// <summary>
+        /// Удалить сущность из репозитория
+        /// </summary>
+        /// <param name="name">имя удаляемой сущности</param>
+        /// <returns>Истина, если удалось удалить</returns>
+        Task<bool> Delete(string name);
+    }
+}
---
+++ WPRMebel.Interfaces.Base/Repositories/IRepository.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WPRMebel.Interfaces.Base.Entities;
+
+namespace WPRMebel.Interfaces.Base.Repositories
+{
+    /// <summary>
+    /// Интерфейс репозитория сущностей
+    /// </summary>
+    /// <typeparam name="TEntity">Сущность</typeparam>
+    public interface IRepository<TEntity> where TEntity : IEntity
+    {
+        /// <summary>
+        /// Существует ли сущность в репозитории
+        /// </summary>
+        /// <param name="id">идентификатор сущности</param>
+        /// <returns>Истина, если сущность есть в репозитории</returns>
+        Task<bool> Exist(int id);
+
+        /// <summary>
+        /// Существует ли сущность в репозитории
+        /// </summary>
+        /// <param name="item">Искомая сущность</param>
+        /// <returns>Истина, если сущность есть в репозитории</returns>
+        virtual Task<bool> Exist(TEntity item) => Exist(item.Id);
+
+        /// <summary> Получить все сущности из репозитория </summary>
+        Task<IEnumerable<TEntity>> GetAll();
+
+        /// <summary> Получить количество сущностей </summary>
+        Task<int> GetCount();
+
+        /// <summary>
+        /// Получить сущность по идентификатору
+        /// </summary>
+        /// <param name="id">Id сущности</param>
+        /// <returns>null, если сущность не найдена</returns>
+        Task<IEntity> GetById(int id);
+
+        /// <summary>
+        /// Добавить сущность в репозиторий
+        /// </summary>
+        /// <param name="item">Добавляемая сущность</param>
+        /// <returns>Добавленная сущность, или null, если добавить не удалось</returns>
+        Task<TEntity> Add(TEntity item);
+
+        /// <summary>
+        /// Обновить сущность в репозитории
+        /// </summary>
+        /// <param name="item">Изменяемая сущность</param>
+        /// <returns>Истина, если удалось обновить</returns>
+        Task<bool> Update(TEntity item);
+
+        /// <summary>
+        /// Удалить сущность из репозитория
+        /// </summary>
+        /// <param name="item">Удаляемая сущность</param>
+        /// <returns>Истина, если удалось удалить</returns>
+        Task<bool> Delete(TEntity item);
+
+        /// <summary>
+        /// Удалить сущность из репозитория
+        /// </summary>
+        /// <param name="id">Id удаляемой сущности</param>
+        /// <returns>Истина, если удалось удалить</returns>
+        Task<bool> Delete(int id);
+    }
+}
