96a83d39770e3320109df44f5c7764aec105cc96
Денис <dultsevdv@gmail.com>
2021-10-31 22:39:12 +0500

    Начало разметки главного окна
    Стиль кнопок главного меню

--- WPRMebel.WPF/Views/Windows/MainWindow.xaml.cs
+++ WPRMebel.WPF/Views/Windows/MainWindow.xaml.cs
@@ -1,7 +1,16 @@
-﻿namespace WPRMebel.WPF.Views.Windows
+﻿using System.Windows;
+using WPR;
+
+namespace WPRMebel.WPF.Views.Windows
 {
     public partial class MainWindow
     {
         public MainWindow() => InitializeComponent();
+
+        private void ButtonBase_OnClick(object Sender, RoutedEventArgs E)
+        {
+            //TODO: убрать обработчик
+           Design.SetNewRandomStyle();
+        }
     }
 }
